e5cd30e0ece2b5b48f24212133d4abfa5ac0a713
Tom Crane <tom@nanoplanet.com>
2022-03-20 17:24:13 +0000

    5-6 cases demo

--- me/anno-paging.js
+++ me/anno-paging.js
@@ -8,31 +8,79 @@ function $(s) {
     }
 }
 
-for(rad of $(".case-1-check")){
+document.addEventListener("DOMContentLoaded", function(event) { 
+    for(ul of $(".nav-tabs")){
+        ul.innerHTML = $("#tabsTemplate").innerHTML;
+    }
+    for(nap of $(".new-anno-page-launcher")){
+        nap.addEventListener("click", function(e){
+            e.preventDefault();
+            showNewAnnoPage(this.getAttribute("data-resource-editor"));
+        });
+    } 
+    for(btn of $(".convert-to-internal")){
+        btn.addEventListener("click", function(e){
+            e.preventDefault();
+            alert("This will create an additional anno page.");
+        });
+    }    
+    for(ana of $(".add-new-annotation")){
+        ana.addEventListener("click", function(e){
+            e.preventDefault();
+            const container = document.getElementById(this.getAttribute("data-anno-container"));
+            const itemHeader = document.getElementById(this.getAttribute("data-anno-items-header"));
+            const template = document.getElementById("exampleAnno").innerHTML;
+            const newAnnoCount = parseInt(container.getAttribute("data-anno-count")) + 1;
+            alert("This would show the individual anno form taking up the whole RHS panel.");
+            container.setAttribute("data-anno-count", newAnnoCount);
+            if(newAnnoCount > 1){
+                container.innerHTML += template;
+            } else {
+                container.innerHTML = template;
+            }
+            itemHeader.innerText = "Items (" + newAnnoCount + ")";
+        });
+    }
+
+    
+});
+
+$("#toggleComments").addEventListener("click", () => {
+    const style = $("#toggleComments").checked ? "block" : "none";
+    for(p of $(".very-small")){
+        p.style.display = style;
+    }
+});
+
+function showNewAnnoPage(resourceEditorId){
+    window.resourceEditorId = resourceEditorId;
+    moveElementContents(resourceEditorId, "reHolder");
+    moveElementContents("newAnnotationPageTemplate", resourceEditorId);
+}
+
+function moveElementContents(sourceElementId, targetElementId){
+    const sourceElement = document.getElementById(sourceElementId);
+    const targetElement = document.getElementById(targetElementId);
+    while (sourceElement.childNodes.length > 0) {
+        targetElement.appendChild(sourceElement.childNodes[0]);
+    }
+}
+
+
+for(rad of $(".new-anno-page-check")){
     rad.addEventListener("change", () => {                
-        const annoPageType = $("#case1Form").elements["radPage"].value;
-        if(annoPageType == "embedded"){            
-            $("#case1Intro").style.display = "none";
-            $("#case1Add").style.display = "block";
-            $("#case1Embedded").style.display = "block";
-            $("#case1RefUrl").style.display = "none";                    
+        if($("#radEmbedded").checked){            
+            $("#newAnnoPageCreate").innerText = "Create embedded annotation page";
+            $("#newAnnoPageRefUrl").style.display = "none";                    
         } else {
-            $("#case1Intro").style.display = "none";
-            $("#case1Add").style.display = "none";
-            $("#case1Embedded").style.display = "none";
-            $("#case1RefUrl").style.display = "block";
+            $("#newAnnoPageCreate").innerText = "Create link to external annotation page";
+            $("#newAnnoPageRefUrl").style.display = "block";
         }
     });
 }
 
-$("#case1AddNewAnno").addEventListener("click", () => {
-    alert("will skip the full annotation editor here...\n\nYou'll see the mini version as if we had created it.")
-    $("#case1BeforeAnno").style.display = "none";
-    $("#case1AfterAnno").style.display = "block";
-    $("#case1Radios").style.display = "none";
-});
-
-$("#case1CreateExternalLink").addEventListener("click", () => {
-    alert("See Case 2 for what this looks like")
-    $("#case1Radios").style.display = "none";
+$("#newAnnoPageCreate").addEventListener("click", () => {
+    alert("This will create the appropriate anno page case (2-6)");    
+    moveElementContents(window.resourceEditorId, "newAnnotationPageTemplate");
+    moveElementContents("reHolder", resourceEditorId);
 });
